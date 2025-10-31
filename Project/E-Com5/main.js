db78a51e2860cb11cf5093ab28727c784273abf6
Ricky M. Vale <rickymoses86@gmail.com>
2025-10-31 12:03:31 +0530

    Shop

--- Project/E-Com5/main.js
+++ Project/E-Com5/main.js
@@ -2,6 +2,8 @@
 const headerEl = document.querySelector("header");
 // Main Element
 const mainEl = document.querySelector("main");
+// Cart or Aside Element
+const asideEl = document.querySelector("aside");
 
 // Function to navigate page
 function navigatePage(pageID = "home") {
@@ -10,7 +12,7 @@ function navigatePage(pageID = "home") {
   });
 }
 
-navigatePage("home");
+navigatePage("shop");
 
 // Home navigation
 headerEl.querySelector("h1").addEventListener("click", () => {
@@ -20,8 +22,13 @@ headerEl.querySelector("h1").addEventListener("click", () => {
 // Shop and Profile navigation
 headerEl.querySelectorAll("nav ul li").forEach((li) => {
   li.addEventListener("click", () => {
-    const dataId = li.dataset.page
-    console.info(dataId)
+    const dataId = li.dataset.page;
+    console.info(dataId);
     navigatePage(dataId);
   });
+});
+
+// Cart click function
+headerEl.querySelector("#cartIcon").addEventListener("click", () => {
+  asideEl.style.display = asideEl.style.display === "none" ? "block" : "none";
 });
