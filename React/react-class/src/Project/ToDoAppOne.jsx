67b317934278ce9bedd218692d44f100b59e8c3f
Ricky M. Vale <rickymoses86@gmail.com>
2025-11-07 12:30:42 +0530

    edit

--- React/react-class/src/Project/ToDoAppOne.jsx
+++ React/react-class/src/Project/ToDoAppOne.jsx
@@ -7,17 +7,42 @@ const ToDoAppOne = () => {
     email: "",
     phone: "",
   });
+  const [editId, setEditId] = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setUserData((prev) => [...prev, { id: userData.length + 1, ...formData }]);
-    console.info(userData)
+    if (editId !== null) {
+      setUserData((prev) =>
+        prev.map((list) =>
+          list.id === editId ? { ...list, ...formData } : list
+        )
+      );
+      setEditId(null);
+    } else {
+      setUserData((prev) => [
+        ...prev,
+        { id: userData.length + 1, ...formData },
+      ]);
+    }
+    // console.info(userData);
   };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     // console.info(`${name} : ${value}`)
     setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleEdit = (id) => {
+    const listFound = userData.find((fin) => fin.id === id);
+
+    setFormData({
+      name: listFound.name,
+      email: listFound.email,
+      phone: listFound.phone,
+    });
+    setEditId(id);
+    // console.info(id);
   };
   return (
     <>
@@ -31,7 +56,7 @@ const ToDoAppOne = () => {
               <input
                 type="text"
                 className="border w-full px-2! h-10"
-                // value={formData.name}
+                value={formData.name}
                 name="name"
                 placeholder="Enter a name"
                 onChange={handleChange}
@@ -41,7 +66,7 @@ const ToDoAppOne = () => {
               <input
                 type="email"
                 className="border w-full px-2! h-10"
-                // value={formData.email}
+                value={formData.email}
                 name="email"
                 placeholder="Enter a email"
                 onChange={handleChange}
@@ -51,7 +76,7 @@ const ToDoAppOne = () => {
               <input
                 type="tel"
                 className="border w-full px-2! h-10"
-                // value={formData.phone}
+                value={formData.phone}
                 name="phone"
                 placeholder="Enter a number"
                 onChange={handleChange}
@@ -69,18 +94,19 @@ const ToDoAppOne = () => {
         <section className="flex-1 overflow-y-scroll">
           <table className=" w-full">
             <tbody className="">
-              {Array.from({ length: 30 }).map((list, idx) => (
-                <tr>
-                  <th className="p-2! mb-1! shadow-2xl">{idx + 1}</th>
-                  <td className="p-2! mb-1! shadow-2xl">User name</td>
-                  <td className="p-2! mb-1! shadow-2xl">
-                    <button>Edit</button>
-                  </td>
-                  <td className="p-2! mb-1! shadow-2xl">
-                    <button>Delete</button>
-                  </td>
-                </tr>
-              ))}
+              {userData.length > 0 &&
+                userData.map((list, idx) => (
+                  <tr>
+                    <th className="p-2! mb-1! shadow-2xl">{idx + 1}</th>
+                    <td className="p-2! mb-1! shadow-2xl">{list.name}</td>
+                    <td className="p-2! mb-1! shadow-2xl">
+                      <button onClick={() => handleEdit(list.id)}>Edit</button>
+                    </td>
+                    <td className="p-2! mb-1! shadow-2xl">
+                      <button>Delete</button>
+                    </td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </section>
