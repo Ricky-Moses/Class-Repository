28ad43fa5a47e19a9d90d61715d31fb776e57416
Ricky M. Vale <rickymoses86@gmail.com>
2025-11-26 11:08:05 +0530

    E-App-Two

--- React/react-class/src/App.jsx
+++ React/react-class/src/App.jsx
@@ -1,42 +1,43 @@
 import React from "react";
+import EAppTwo from "./Project/E-Commerce-2/EAppTwo";
 // import EApp from "./Project/E-Commerce/EApp";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Form from "./ClassTwo/Form";
-import Register from "./ClassTwo/Auth/Register";
-import List from "./ClassTwo/Auth/List";
-import FetchUser from "./ClassTwo/Controllers/FetchUser";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import Form from "./ClassTwo/Form";
+// import Register from "./ClassTwo/Auth/Register";
+// import List from "./ClassTwo/Auth/List";
+// import FetchUser from "./ClassTwo/Controllers/FetchUser";
 
 const App = () => {
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Form />,
-      children: [
-        {
-          index: true,
-          element: <Register />,
-        },
-        {
-          path: "list",
-          element: <List />,
-        },
-        {
-          path: "edit/:id",
-          element: <Register />,
-        },
-      ],
-    },
-  ]);
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <Form />,
+  //     children: [
+  //       {
+  //         index: true,
+  //         element: <Register />,
+  //       },
+  //       {
+  //         path: "list",
+  //         element: <List />,
+  //       },
+  //       {
+  //         path: "edit/:id",
+  //         element: <Register />,
+  //       },
+  //     ],
+  //   },
+  // ]);
 
   return (
     <>
-
-      <FetchUser>
+      {/* <FetchUser>
         <RouterProvider router={router} />
-      </FetchUser>
+      </FetchUser> */}
 
       {/* <EApp /> */}
+
+      <EAppTwo />
     </>
   );
 };
---
+++ React/react-class/src/Project/E-Commerce-2/EAppTwo.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layouts from "./Layouts/Layouts";
+import Home from "./Pages/Home";
+import Shop from "./Pages/Shop";
+import About from "./Pages/About";
+import Profile from "./Pages/Profile";
+
+const EAppTwo = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layouts />,
+      children: [
+        {
+            index: true,
+            element: <Home />
+        },
+        {
+            path: "shop",
+            element: <Shop />
+        },
+        {
+            path: "about",
+            element: <About />
+        },
+        {
+            path: "profile",
+            element: <Profile />
+        }
+      ]
+    },
+  ]);
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+};
+
+export default EAppTwo;
