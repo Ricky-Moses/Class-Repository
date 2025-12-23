1662f15c137af89a956f5c8209dc311194040d1d
Ricky M. Vale <rickymoses86@gmail.com>
2025-12-23 13:09:49 +0530

    project

--- Project/E-Com6/main.js
+++ Project/E-Com6/main.js
@@ -2,6 +2,20 @@ const headerEl = document.querySelector("header");
 const mainEl = document.querySelector("main");
 const asideEl = document.querySelector("aside");
 
+// Fetch data from 'data.json'
+async function fetchData() {
+  try {
+    const res = await fetch("./asset/data.json");
+    const data = await res.json();
+    renderData(data);
+    // console.info(data);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+fetchData();
+
 // Show pages function
 const allPages = mainEl.querySelectorAll("section");
 // console.info(allPages);
@@ -13,7 +27,7 @@ function showPages(pageId) {
   });
 }
 
-showPages("home");
+showPages("shop");
 
 // Click to 'home' page
 const homePage = headerEl.querySelector("h1");
@@ -36,14 +50,45 @@ function showCart(display) {
   asideEl.classList.toggle(display);
 }
 
-const isCartOpen = headerEl.querySelector("i")
+const isCartOpen = headerEl.querySelector("i");
 isCartOpen.addEventListener("click", () => {
-  showCart("!block")
-})
+  showCart("!block");
+});
 
 // Cart item (Function)
-function cartItems(){
-  const ulEl = asideEl.querySelector("ul")
+function cartItems() {
+  const ulEl = asideEl.querySelector("ul");
+}
 
-  
+// Render data (Function)
+const shopPage = mainEl.querySelector("#shop");
+function renderData(data) {
+  // console.info(data);
+  const ulEl = shopPage.querySelector("ul");
+
+  ulEl.innerHTML = `
+    ${Object.keys(data)
+      .map(
+        (keyName) => `
+        <li class="capitalize" data-key="${keyName}">${keyName
+          .split("_")
+          .join(" ")}</li>
+      `
+      )
+      .join("")}
+  `;
+
+  const navigateProduct = ulEl.querySelectorAll("li");
+  // console.info(navigateProduct)
+  navigateProduct.forEach((list) => {
+    list.addEventListener("click", () => {
+      productCard(data[list.dataset.key]);
+    });
+  });
+}
+
+// Shop product card (Function)
+function productCard(data) {
+  console.info(data)
+  const cardContainer = shopPage.querySelector("#cardContainer");
 }
